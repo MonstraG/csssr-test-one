b92305311f1f2791881bc389994d2712ab5add7c
Arseniy Garelyshev <Ars.garelyshev@yandex.ru>
2020-07-23 18:09:58 +0500

    Done

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@
 // При нажатии на "старт" должен запускаться секундомер и через заданный интервал времени увеличивать свое значение на значение интервала
 // При нажатии на "стоп" секундомер должен останавливаться и сбрасывать свое значение
 
-import * as React from "react";
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
+//should we be able to change currentTime while it's running?
+
+import * as React from "react"
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
 
 const createStore = (reducer, initialState) => {
   let currentState = initialState
@@ -39,8 +41,13 @@ const connect = (mapStateToProps, mapDispatchToProps) =>
         )
       }
 
-      componentDidUpdate() {
-        this.context.store.subscribe(this.handleChange)
+      //mount/unmount subscription to be able to listen to state changes
+      componentDidMount() {
+        this.unsubscribe = this.context.store.subscribe(this.handleChange.bind(this))
+      }
+
+      componentWillUnmount() {
+        this.unsubscribe()
       }
 
       handleChange = () => {
@@ -87,7 +94,9 @@ const changeInterval = value => ({
 const reducer = (state, action) => {
   switch(action.type) {
     case CHANGE_INTERVAL:
-      return state += action.payload
+      // actually assigning state as object, not as primitive.
+      // + we don't need negative timer values
+      return {...state, currentInterval: Math.max(0, state.currentInterval + action.payload)}
     default:
       return {}
   }
@@ -109,16 +118,25 @@ class IntervalComponent extends React.Component {
   }
 }
 
-const Interval = connect(dispatch => ({
-    changeInterval: value => dispatch(changeInterval(value)),
-  }),
+const Interval = connect( //switched them around, they were in the wrong order
   state => ({
-    currentInterval: state,
+    currentInterval: state.currentInterval,
+  }), dispatch => ({
+    changeInterval: value => dispatch(changeInterval(value)),
   }))(IntervalComponent)
 
 class TimerComponent extends React.Component {
   state = {
-    currentTime: 0
+    currentTime: 0,
+    timerId: null //added timerId to be able to clear it
+  }
+
+  //constructor to bing this in fns
+  constructor(props) {
+    super(props)
+
+    this.handleStart = this.handleStart.bind(this)
+    this.handleStop = this.handleStop.bind(this)
   }
 
   render() {
@@ -137,24 +155,36 @@ class TimerComponent extends React.Component {
   }
 
   handleStart() {
-    setTimeout(() => this.setState({
+    // don't need to start dispatching as many events as possible
+    if (this.props.currentInterval === 0) {
+      return;
+    }
+    const timerId = setInterval(() => this.setState({
       currentTime: this.state.currentTime + this.props.currentInterval,
-    }), this.props.currentInterval)
+    }), this.props.currentInterval * 1000) //setInterval takes in ms, not s
+    this.setState({timerId: timerId}) // remembering to be able to clear it
   }
 
   handleStop() {
-    this.setState({ currentTime: 0 })
+    //clear interval if was set
+    if (this.state.timerId !== null) {
+      clearInterval(this.state.timerId)
+    }
+    this.setState({ currentTime: 0, timerId: null })
   }
 }
 
 const Timer = connect(state => ({
-  currentInterval: state,
+  currentInterval: state.currentInterval, //again, state is not primitve, take out value from the object
 }), () => {})(TimerComponent)
 
 // init
+// actually setting initialState here
 ReactDOM.render(
-  <Provider store={createStore(reducer)}>
+  <Provider store={createStore(reducer, {currentInterval: 0})}>
     <Timer/>
   </Provider>,
   document.getElementById('app')
-);
+)
+
+// tbh I prefer ;'s at the end of line
